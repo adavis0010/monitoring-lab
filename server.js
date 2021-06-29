f33bbb7cd45ed531273037b0dc613b149fb291c1
Ash Davis <sadavis0010@gmail.com>
2021-06-29 13:12:53 -0700

    rollbar

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const rollbar = require('rollbar')
 const path = require('path');
 
 const app = express();
 
 app.use(express.json())
 
+const rollbar = new Rollbar({
+    accessToken: '240b6d6fad95494cb0fbebf41a42094c',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+  });
+
 app.get('/', function(req,res){
+    rollbar.log("Hello World!")
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
